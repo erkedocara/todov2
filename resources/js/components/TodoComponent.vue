645a628d9a9ae38929f5b70ab44b3bbe9f5e5d54
Erkedo Cara <erkedo.cara@sisal.al>
2021-02-08 20:46:37 +0100

    first commit

---
+++ resources/js/components/TodoComponent.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="w-25">
+        <form>
+    <div class="input-group mb-3 w-100">
+        <input v-model="form.title" type="text" class="form-control form-control-lg"  aria-label="Recipient's username" aria-describedby="button-addon2">
+        <div class="input-group-append">
+            <button class="btn btn-success" type="submit" id="basic-addon2">Add this to your list</button>
+        </div>
+    </div>
+        </form>
+    </div>
+</template>
+
+<script>
+    export default {
+       data(){
+           return{
+               form:new Form({
+                   title:'',
+               })
+           }
+       }
+    }
+</script>
